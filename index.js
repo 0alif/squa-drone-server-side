9400d95b95faeff95b1c72811b5216633f399668
0alif <alifhossain003@gmail.com>
2021-11-18 00:13:52 +0600

    admin

--- index.js
+++ index.js
@@ -69,6 +69,17 @@ async function run() {
             res.json(result);
         })
 
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false
+            if (user?.role === 'admin') {
+                isAdmin = true
+            }
+            res.json({ admin: isAdmin });
+        })
+
         // user api
         app.post('/users', async (req, res) => {
             const user = req.body;
@@ -83,6 +94,15 @@ async function run() {
             const options = { upsert: true };
             const updateDoc = { $set: user };
             const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        })
+
+        // make admin
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
             res.json(result);
         })
 
